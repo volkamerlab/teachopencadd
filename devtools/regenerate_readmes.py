23607ff7b1ad3b4e67c9a3760a7f00bc2abaa0f5
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2020-11-14 13:39:26 +0100

    Simpler usage for one-off cases + docs

--- devtools/regenerate_readmes.py
+++ devtools/regenerate_readmes.py
@@ -1,6 +1,15 @@
-"""
+r"""
 This script takes our talktorials and renders the first markdown cell
 of the notebook to the corresponding README.md in the containing folder.
+
+Usage
+-----
+
+.. code-block::
+
+    % python devtools/regenerate_readmes.py --output README.md \
+        teachopencadd/talktorials/001_query_chembl/talktorial.ipynb
+
 """
 
 
@@ -15,10 +24,30 @@ def first_markdown_cells(path, stopif=lambda cell: "## Theory" in cell["source"]
             yield cell["source"]
 
 
-if __name__ == "__main__":
-    import sys
+def parse_cli():
+    from argparse import ArgumentParser
 
-    nbcontent = "\n".join(list(first_markdown_cells(sys.argv[1])))
+    p = ArgumentParser()
+    p.add_argument("notebook", type=str)
+    p.add_argument("--output", type=str, default=None)
+    return p.parse_args()
+
+
+def main():
+    from pathlib import Path
+
+    args = parse_cli()
+
+    nbcontent = "\n".join(list(first_markdown_cells(args.notebook)))
     if not nbcontent.strip():  # empty results
         nbcontent = "> This talktorial is still under development"
-    print(nbcontent)
+
+    if args.output:
+        with open(Path(args.notebook).parent / args.output, "w") as f:
+            f.write(nbcontent + "\n")
+    else:
+        print(nbcontent)
+
+
+if __name__ == "__main__":
+    main()
