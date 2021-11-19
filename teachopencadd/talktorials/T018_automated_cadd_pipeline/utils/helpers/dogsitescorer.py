12f9db2db717b65fd42a9dab0df66b1f455e2202
Armin-Ariamajd <80158628+Armin-Ariamajd@users.noreply.github.com>
2021-11-19 06:18:19 +0100

    improved `dogsitescorer.py` and resolved FIXME tag
    Shortened the `upload_pdb_file` and `submit_job` functions by isolating the repetitive part and putting it in a new function `_send_request_get_results`.
    
    Replaced `while` loop for repeating API request attempts with a `@redo.retriable` decorator.
    
    Replaced the `try-except` block in `calculate_pocket_coordinates_from_pocket_pdb_file` (for selecting float-strings from list of strings) with regex expression.
    
    Modified `get_pocket_residues` to take in local pdb filepath instead of DoGSiteScorer URL.
    
    Added missing `.raise_for_status()` to requests.

--- teachopencadd/talktorials/T018_automated_cadd_pipeline/utils/helpers/dogsitescorer.py
+++ teachopencadd/talktorials/T018_automated_cadd_pipeline/utils/helpers/dogsitescorer.py
@@ -17,11 +17,13 @@ several methods for selecting the most suitable binding site.
 import io  # for creating file-like objects from strings (needed as input for some functions)
 import gzip  # for decompressing .gz files downloaded from DoGSiteScorer
 import time  # for creating pauses during runtime (e.g. to wait for the response of API requests)
-from pathlib import Path
+from pathlib import Path # for handling local paths
+import re # for filtering floats from a list of strings
 
 import requests  # for communicating with web-service APIs
 import pandas as pd  # for creating dataframes and handling data
 from biopandas.pdb import PandasPdb  # for working with PDB files
+import redo # for retrying API queries if they fail
 
 from . import pdb
 
@@ -63,7 +65,54 @@ class APIConsts:
             "pockets_ccp4_files": "pockets",
         }
 
-
+@redo.retriable(attempts=30, sleeptime=1, sleepscale=1.1, max_sleeptime=20)
+def _send_request_get_results(
+    request_type, 
+    keys_list, 
+    url, 
+    task="Fetching results from DoGSiteScorer API", 
+    **kwargs
+):
+    '''
+    Send a request and get the keyword values from json response.
+    
+    Parameters
+    ----------
+    request_type : str
+        Type of request, i.e. name of a function from the `requests` module,
+        e.g. "get", "post".
+    keys_list : list of strings
+        List of keys in the json response to return.
+    url : str
+        URL to send the request to.
+    task : str
+        Textual description of the request's purpose to print in the error message if one is raised.
+        Optional; default : "Fetching results from DoGSiteScorer API"
+    **kwargs
+        Additional arguments to send with the request.
+    
+    Returns
+    -------
+    list
+        List of values in the json response corresponding to the input list of keys. 
+    '''
+    
+    request_function = getattr(requests, request_type)
+    response = request_function(url, **kwargs)
+    response.raise_for_status()
+    response_values = response.json()
+    results=[]
+    for key in keys_list:
+        try:
+            results.append(response_values[key])
+        except KeyError:
+            raise ValueError(
+                f"{task} failed.\n"
+                +f"Expected key {key} not found in the response.\n"
+                +f"The response message is as follows: {response_values}"
+            )
+    return results
+    
 def upload_pdb_file(filepath):
     """
     Upload a PDB file to the DoGSiteScorer webserver using their API
@@ -80,61 +129,24 @@ def upload_pdb_file(filepath):
         Dummy PDB code of the uploaded structure, which can be used instead of a PDB code.
     """
 
-    filepath = Path(filepath)
-
-    # Read API URL and API request message from Constants
-    url = APIConsts.FileUpload.URL
-    request_msg = APIConsts.FileUpload.REQUEST_MSG
-
     # Open the local PDB file for reading in binary mode
-    with open(filepath, "rb") as f:
+    with open(Path(filepath), "rb") as f:
         # Post API query and get the response
-        response = requests.post(url, files={request_msg: f})
-    # Raise HTTPError if one occured during query
-    response.raise_for_status()
-    # Turn the response values into a dict
-    response_values = response.json()
-    if response.ok:
-        # If the request is accepted, the response will contain a URL,
-        # from which the needed ID of the uploaded protein can be obtained.
-        # Here, we store this URL from the response values in the url_of_id variable.
-        url_of_id = response_values[APIConsts.FileUpload.RESPONSE_MSG["url_of_id"]]
-    else:
-        raise ValueError(
-            "Uploading PDB file failed.\n"
-            + f"The response values are as follows: {response_values}"
-        )
-    # After getting the URL, it may take some time for the server to process the uploaded file
-    # and return an ID. Thus, we try 30 times in intervals of 5 seconds to query the URL,
-    # until we get the ID
-    for try_nr in range(30):
-        # Query the URL containing the ID
-        id_response = requests.get(url_of_id)
-        # Raise HTTPError if one occured during query
-        id_response.raise_for_status()
-        # Turn the response values into a dict
-        id_response_values = id_response.json()
-        # The response should contain the `id` keyword:
-        if id_response.ok and (
-            APIConsts.FileUpload.RESPONSE_MSG_FETCH_ID["id"] in id_response_values
-        ):
-            id_response_values = id_response.json()
-            protein_id = id_response_values[APIConsts.FileUpload.RESPONSE_MSG_FETCH_ID["id"]]
-            break
-        else:
-            time.sleep(5)
-
-    if not (
-        id_response.ok and (APIConsts.FileUpload.RESPONSE_MSG_FETCH_ID["id"] in id_response_values)
-    ):
-        raise ValueError(
-            "Fetching the ID of uploaded protein failed.\n"
-            + f"The response values are as follows: {id_response_values}"
-        )
+        url_of_id = _send_request_get_results(
+            "post",
+            [APIConsts.FileUpload.RESPONSE_MSG["url_of_id"]],
+            APIConsts.FileUpload.URL, 
+            files={APIConsts.FileUpload.REQUEST_MSG: f}
+        )[0]
+    
+    protein_id = _send_request_get_results(
+        "get",
+        [APIConsts.FileUpload.RESPONSE_MSG_FETCH_ID["id"]],
+        url_of_id
+    )[0]
     return protein_id
 
-
-def submit_job(pdb_id, ligand_id="", chain_id="", num_attempts=30):
+def submit_job(pdb_id, ligand_id="", chain_id=""):
     """
     Submit a protein structure to DoGSiteScorer webserver using their API
     and get back all the information on the detected binding sites.
@@ -150,18 +162,17 @@ def submit_job(pdb_id, ligand_id="", chain_id="", num_attempts=30):
         <PDB ligand-ID>_<chain-ID>_<PDB residue number of the ligand>
     chain_id : str (optional; default: none)
         Chain ID to limit the binding site detection to.
-    num_attempts : int
-        Optional; default: 30.
-        Number of times to attempt to fetch the results after the job has been submitted.
-        After each failed attempt there is a 10 second pause.
 
     Returns
     -------
     pandas.DataFrame
         DataFrame containing all the information on all detected binding sites.
     """
-    response = requests.post(
-        APIConsts.SubmitJob.URL,
+    
+    url_of_job = _send_request_get_results(
+        "post",
+        [APIConsts.SubmitJob.RESPONSE_MSG["url_of_job"]],
+        APIConsts.SubmitJob.URL, 
         json={
             "dogsite": {
                 "pdbCode": pdb_id,  # PDB code of protein
@@ -171,46 +182,22 @@ def submit_job(pdb_id, ligand_id="", chain_id="", num_attempts=30):
                 "chain": chain_id,  # if specified, calculation is only performed on this chain
             }
         },
-        headers=APIConsts.SubmitJob.QUERY_HEADERS,
-    )
+        headers=APIConsts.SubmitJob.QUERY_HEADERS
+    )[0]
+
+    (binding_site_data_url, 
+     binding_sites_pdb_files_urls, 
+     binding_sites_ccp4_files_urls) = _send_request_get_results(
+        "get",
+        [APIConsts.SubmitJob.RESPONSE_MSG_FETCH_BINDING_SITES["result_table"],
+         APIConsts.SubmitJob.RESPONSE_MSG_FETCH_BINDING_SITES["pockets_pdb_files"],
+         APIConsts.SubmitJob.RESPONSE_MSG_FETCH_BINDING_SITES["pockets_ccp4_files"]
+        ],
+        url_of_job)
+    
+    response = requests.get(binding_site_data_url)
     response.raise_for_status()
-    response_values = response.json()
-    url_of_job = response_values[APIConsts.SubmitJob.RESPONSE_MSG["url_of_job"]]
-
-    attempt_count = 0
-    while attempt_count <= num_attempts:
-        job_response = requests.get(url_of_job)
-        job_response.raise_for_status()
-        job_response_values = job_response.json()
-
-        if (
-            APIConsts.SubmitJob.RESPONSE_MSG_FETCH_BINDING_SITES["result_table"]
-            in job_response_values
-        ):
-            binding_site_data_url = job_response_values[
-                APIConsts.SubmitJob.RESPONSE_MSG_FETCH_BINDING_SITES["result_table"]
-            ]
-            binding_sites_pdb_files_urls = job_response_values[
-                APIConsts.SubmitJob.RESPONSE_MSG_FETCH_BINDING_SITES["pockets_pdb_files"]
-            ]
-            binding_sites_ccp4_files_urls = job_response_values[
-                APIConsts.SubmitJob.RESPONSE_MSG_FETCH_BINDING_SITES["pockets_ccp4_files"]
-            ]
-            break
-        else:
-            # FIXME action needed?
-            pass
-
-        attempt_count += 1
-        time.sleep(10)
-    else:
-        raise ValueError(
-            "Fetching the binding site data failed.\n"
-            + f"The response values are as follows: {job_response_values}"
-        )
-
-    binding_site_data_table = requests.get(binding_site_data_url).text
-    binding_site_data_file = io.StringIO(binding_site_data_table)
+    binding_site_data_file = io.StringIO(response.text)
     binding_site_df = pd.read_csv(binding_site_data_file, sep="\t").set_index("name")
     binding_site_df["pdb_file_url"] = binding_sites_pdb_files_urls
     binding_site_df["ccp4_file_url"] = binding_sites_ccp4_files_urls
@@ -229,8 +216,6 @@ def save_binding_sites_to_file(binding_site_df, output_path):
         Local file path to save the files in.
     """
 
-    output_path = Path(output_path)
-
     for binding_site in binding_site_df.index:
         for column in ["pdb_file_url", "ccp4_file_url"]:
             response = requests.get(binding_site_df.loc[binding_site, column])
@@ -243,7 +228,7 @@ def save_binding_sites_to_file(binding_site_df, output_path):
                 file_extension = ".ccp4"
 
             file_name = binding_site + file_extension
-            with open(output_path / file_name, "wb") as f:
+            with open(Path(output_path) / file_name, "wb") as f:
                 f.write(response_file_content)
     return
 
@@ -311,19 +296,9 @@ def calculate_pocket_coordinates_from_pocket_pdb_file(filepath):
         pdb_file_text_content = f.read()
     pdb_file_df = pdb.load_pdb_file_as_dataframe(pdb_file_text_content)
     pocket_coordinates_data = pdb_file_df["OTHERS"].loc[5, "entry"]
-    coordinates_data_as_list = pocket_coordinates_data.split(" ")
-    # `coordinates_data_as_list` looks like this:
-    # ['\tGeometric', 'pocket', 'center', 'at', '', '15.91', '', '32.33', '', '11.03', 'with', 'max', 'radius', '12.42']
-
-    # Let's keep only coordinates
-    coordinates = []
-    for elem in coordinates_data_as_list:
-        try:
-            # If element can be cast to float (i.e. is a coordinate)
-            coordinates.append(float(elem))
-        except:
-            # If element cannot be cast to float, pass and continue with next list element
-            pass
+    coordinates_data_as_list = pocket_coordinates_data.split()
+    # select strings representing floats from a list of strings
+    coordinates = [float(element) for element in coordinates_data_as_list if re.compile(r'\d+(?:\.\d*)').match(element)]
     pocket_coordinates = {
         "center": coordinates[:3],
         "size": [coordinates[-1] * 2 for dim in range(3)],
@@ -331,24 +306,23 @@ def calculate_pocket_coordinates_from_pocket_pdb_file(filepath):
     return pocket_coordinates
 
 
-def get_pocket_residues(pocket_residues_url):
+def get_pocket_residues(pocket_pdb_filepath):
     """
-    Gets residue IDs and names of a specified pocket (via URL).
+    Get residue-IDs and names of a specified pocket.
 
     Parameters
     ----------
-    pocket_residues_url : str
-        URL of selected pocket file on the DoGSiteScorer web server.
+    pocket_pdb_filepath : str or pathlib.Path
+        Path of pocket's PDB file.
 
     Returns
     -------
     pandas.DataFrame
         Table of residues names and IDs for the selected binding site.
     """
-    # Retrieve PDB file content from URL
-    result = requests.get(pocket_residues_url)
-    # Get content of PDB file
-    pdb_residues = result.text
+    
+    with open(pocket_pdb_filepath) as f:
+        pdb_residues = f.read()
     # Load PDB format as DataFrame
     ppdb = PandasPdb().read_pdb_from_list(pdb_residues.splitlines(True))
     pdb_df = ppdb.df["ATOM"]
@@ -356,4 +330,6 @@ def get_pocket_residues(pocket_residues_url):
     # PDB file contains per atom entries, we only need per residue info
     pdb_df.sort_values("residue_number", inplace=True)
     pdb_df.drop_duplicates(subset="residue_number", keep="first", inplace=True)
+    pdb_df.reset_index(drop=True, inplace=True)
+    pdb_df.index += 1
     return pdb_df[["residue_number", "residue_name"]]
